ee2581f173ac1d3a436496a42dde96643a764ae4
Leonardo Pernomian <leonardopernomian@hotmail.com>
2024-12-01 21:37:19 -0300

    corrigindo imagem

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function Banner() {
   return (
     <div>
-      <Image src="/images/barber-shop-banner.jpg" alt="Banner" layout="responsive" width={1920} height={600} />
+      <Image src="/images/BARBER-SHOP-BANNER.jpg" alt="Banner" layout="responsive" width={1920} height={600} />
     </div>
   );
 }
